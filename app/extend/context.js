8fe68a0e6166fe04f6fd3b0fece0c7b31e91f0de
sqlwwx <wwx@wuweixing.com>
2020-07-14 16:35:38 +0800

    feat: context.sendJsonToOns

--- app/extend/context.js
+++ app/extend/context.js
@@ -4,4 +4,17 @@ module.exports = {
   get ons() {
     return this.app.ons;
   },
+
+  async sendJsonToOns(producerId, json, opts = {}) {
+    const { topic, tag = producerId, ...msgOpts } = opts;
+    const distTag = tag || '';
+    const msg = JSON.stringify({
+      id: shortId.generate(),
+      ...json,
+    });
+    const ret = await this.app.ons.getProducer(producerId, topic)
+      .sendMsg(msg, distTag, msgOpts);
+    this.logger.info('[ons.send]', producerId, topic || '', distTag, msg);
+    return ret;
+  },
 };
